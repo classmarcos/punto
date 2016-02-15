ff1a9efee73b630a29c3e16e1daa26ffd95cef92
classmarcos <marcos.class07@gmail.com>
2016-02-15 13:57:26 -0400

    Agregando view del login

--- modulos/login/views/Login.php
+++ modulos/login/views/Login.php
@@ -1,7 +1,39 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: suarez
- * Date: 15/02/2016
- * Time: 10:40
- */
+
+        $usuario_in = array(
+
+            'name'        =>'usuario',
+            'id'          =>'username',
+            'value'       =>@set_value('usuario'),
+            'class'       =>'form-control',
+            'placeholder' =>'Digiete Usuario',
+
+        );
+
+        $clave = array(
+            'name'        =>'clave',
+            'id'          =>'password',
+            'value'       =>@set_value('clave'),
+            'class'       =>'form-control',
+            'placeholder' =>'Digiete Contraseña',
+
+        );
+?>
+
+<div class="contenedor_login">
+         <?php
+            if(!$this->session->userdata('login'));
+         ?>
+
+        <?php
+            $class = array('id'=>'login', 'class'=>'form-login');
+         ?>
+
+         <?php echo form_open_multipart('login/entrar',$class);?>
+
+    <h2 class="form-login-headingd">
+        <img src="<?php base_url();?> public/img/logo/edos.png" width="80" height="60">
+        Ingrego al Sistema
+    </h2>
+
+</div>
