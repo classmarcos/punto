5723bce44ea88aff376fdcd88e3d17f50c14f805
classmarcos <marcos.class07@gmail.com>
2016-02-15 14:04:46 -0400

    Agregando archivos requerido para el funcionamiento del ligin

--- application/views/plantilla_admin.php
+++ application/views/plantilla_admin.php
@@ -1,7 +1,41 @@
-<?php
-/**
- * Created by PhpStorm.
- * User: suarez
- * Date: 15/02/2016
- * Time: 10:25
- */
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8" />
+    <title><?php echo $titulo ?></title>
+
+
+    <link rel="stylesheet" href="<?php echo base_url()?>public/css/font-awesome.css" />
+    <link rel="stylesheet" href="<?php echo base_url()?>public/bootstrap/css/bootstrap.min.css" />
+    <link rel="stylesheet" href="<?php echo base_url()?>public/bootstrap/css/bootstrap-theme.css" />
+    <link rel="shortcut icon" href="<?php echo base_url()?>favicon.ico" />
+    <link rel="stylesheet" type="text/css" href="<?php echo base_url();?>public/css/login.css">
+
+    <link href="<?php echo base_url();?>assets/css/style.css" rel="stylesheet">
+    <link href="<?php echo base_url();?>assets/css/style-responsive.css" rel="stylesheet">
+
+</head>
+<body>
+<div class="container">
+
+    <div class="col-md-4 col-md-offset-4">
+        <div id="contenido">
+            <?php $this->load->view($contenido) ?>
+        </div>
+    </div>
+</div>
+
+
+<script src="<?php echo base_url()?>public/js/jquery-1.11.3.min.js"></script>
+<script src="<?php echo base_url()?>public/js/ajaxload.js"></script>
+<script src="<?php echo base_url()?>public/bootstrap/js/bootstrap.min.js"></script>
+<script src="<?php echo base_url()?>public/js/login.js"></script>
+<script src="<?php echo base_url()?>public/js/form.js"></script>
+<script type="text/javascript">var base_url = "<?php echo base_url()?>"; </script>
+
+</body>
+</html>
+               
+
+
+        
--- modulos/login/libraries/Validacion.php
+++ modulos/login/libraries/Validacion.php
@@ -1,7 +1,25 @@
-<?php
-/**
- * Created by PhpStorm.
- * User: suarez
- * Date: 15/02/2016
- * Time: 10:39
- */
+<?php if ( ! defined('BASEPATH')) exit('No se permite el acceso direccto a este script Su IP ha sido rastreada');
+
+class Validacion
+{
+
+    public function __construct()
+    {
+        $this->CI = & get_instance();
+    }
+
+
+    function verificar_fecha($fecha)
+    {
+        if ($fecha < date('Y-m-d'))
+        {
+            $this->CI->form_validation->set_message('verificar_fecha', 'Por favor, elige fechas de entrada y de salida a partir del día de hoy');
+            return false;
+        }
+        else
+        {
+            return true;
+        }
+    }
+
+}
